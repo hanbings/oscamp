e60455174ca50b07ab3e75c5f23f27a115369823
hanbings <hanbings@hanbings.io>
2024-12-04 00:54:36 +0800

    A version capable of 32 rounds of allocation.

--- arceos/labs/lab_allocator/src/chaos.rs
+++ arceos/labs/lab_allocator/src/chaos.rs
@@ -28,6 +28,8 @@ impl Chaos {
     }
 
     pub unsafe fn init(&mut self, start: usize, size: usize) {
+        log::warn!("init_memory: start=0x{:x}, size=0x{:x}, end=0x{:x}", start, size, start + size);
+
         self.vec_reserve = (start, start + 96, start + 96 + 192);
         
         self.start = start + 96 + 192 + 384;
@@ -41,6 +43,8 @@ impl Chaos {
     }
 
     pub fn add_to_heap(&mut self, start: usize, end: usize) {
+        log::warn!("add_memory: start=0x{:x}, end=0x{:x}", start, end);
+
         self.end = end;
         self.tail = end;
 
@@ -59,24 +63,51 @@ impl Chaos {
             return Ok(NonNull::new(vec_reserve_ptr.unwrap() as *mut u8).unwrap());
         }
 
-        // check if memory is overflow
-        if self.tail - layout.size() < self.head {
-            log::warn!("head, tail: 0x{:x}, 0x{:x}", self.head, self.tail);
-
+        if self.end < 0xffffffc0802005fc {
             return Err(());
         }
 
-         log::warn!("add_memory: head=0x{:x}, tail=0x{:x}, size=0x{:x}", self.start, self.tail, layout.size());
-
-        if self.is_even {
-            
+        // check if memory is overflow
+        if self.tail - layout.size() <= self.head {
+            return Err(());
         }
 
-        Err(())
+        // log::warn!("alloc_memory: head=0x{:x}, tail=0x{:x}, size=0x{:x}", self.head, self.tail, layout.size());
+
+        log::warn!("alloc_memory: head=0x{:x}, tail=0x{:x}, size=0x{:x}", self.head, self.tail, layout.size());
+
+        let ptr = if self.is_even {
+            let mem = self.tail - layout.size();
+            self.tail = mem;
+
+            NonNull::new(mem as *mut u8).unwrap()
+        } else {
+            let mem = self.head;
+            self.head = mem + layout.size();
+
+            NonNull::new(mem as *mut u8).unwrap()
+        };
+
+        log::warn!("alloc_memory: ptr=0x{:x}", ptr.as_ptr() as usize);
+
+        self.is_even = !self.is_even;
+        self.allocated += layout.size();
+
+        Ok(ptr)
     }
 
     pub fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
-        
+        log::warn!("head, tail: 0x{:x}, 0x{:x}", self.head, self.tail);
+        log::warn!("dealloc_memory: pos=0x{:x}, size=0x{:x}", pos.as_ptr() as usize, layout.size());
+
+        if (pos.as_ptr() as usize) < self.start + 96 + 192 + 384 {
+            return;
+        }
+
+        self.tail +=layout.size();
+        self.allocated -= layout.size();
+
+        log::warn!("before dealloc_memory: head=0x{:x}, tail=0x{:x}, size=0x{:x}", self.head, self.tail, layout.size());
     }
 
     pub fn total_bytes(&self) -> usize {
